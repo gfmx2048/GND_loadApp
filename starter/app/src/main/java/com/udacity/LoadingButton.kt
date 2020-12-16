8f40dc61e48d609d1f37a242684bca15c70d8658
vasilis <viktoratosv@gmail.com>
2020-12-16 16:54:45 +0200

    misc

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -5,21 +5,27 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
-
-    private val valueAnimator = ValueAnimator()
-
-    private val pointPosition: PointF = PointF(0.0f,0.0f)
+    private var label = context.getString(R.string.button_name)
+    private var rectBackgroundColor = ContextCompat.getColor(context,R.color.colorPrimary)
+    private var rectForegroundColor = ContextCompat.getColor(context,R.color.colorPrimaryDark)
+    private var circeColor = ContextCompat.getColor(context,R.color.colorAccent)
+    private var labelColor = ContextCompat.getColor(context,R.color.colorBlack)
+    private var radiusOfCircle = 0.0f
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
         textSize = 55.0f
         typeface = Typeface.create("",Typeface.BOLD)
     }
+
+    private val valueAnimator = ValueAnimator()
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
         // if ButtonState.Loading -> show "Loading" text and start the animation
@@ -33,23 +39,40 @@ class LoadingButton @JvmOverloads constructor(context: Context, attrs: Attribute
 
 
     init {
+        isClickable = true // enable view to accept user input
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            rectBackgroundColor = getColor(R.styleable.LoadingButton_rect_backgroundColor, ContextCompat.getColor(context,R.color.colorPrimary))
+            rectForegroundColor = getColor(R.styleable.LoadingButton_rect_foregroundColor, ContextCompat.getColor(context,R.color.colorPrimaryDark))
+            circeColor = getColor(R.styleable.LoadingButton_circleColor, ContextCompat.getColor(context,R.color.colorAccent))
+            labelColor = getColor(R.styleable.LoadingButton_labelColor, ContextCompat.getColor(context,R.color.colorBlack))
+            label = getString(R.styleable.LoadingButton_label)?:context.getString(R.string.button_name)
+        }
+    }
 
+    override fun performClick(): Boolean {
+        if(super.performClick()) return true
+        label = context.getString(R.string.button_loading)
+        invalidate()
+        return true
     }
 
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        paint.color = Color.GREEN
+        paint.color = rectBackgroundColor
         canvas?.drawRect(0f,0f,widthSize.toFloat(),heightSize.toFloat(),paint)
-        paint.color = Color.YELLOW
-        canvas?.drawCircle((widthSize/2).toFloat(),(heightSize/2).toFloat(),40f,paint)
-        paint.color = Color.BLACK
-        canvas?.drawText("TEst text", 0f,0f,paint)
+        paint.color = rectForegroundColor
+        canvas?.drawRect(0f,0f,widthSize.div(3).toFloat(),heightSize.toFloat(),paint)
+        paint.color = circeColor
+        canvas?.drawCircle(widthSize.div(2).toFloat(),heightSize.div(2).toFloat(),radiusOfCircle,paint)
+        paint.color = labelColor
+        canvas?.drawText(label, 80f ,heightSize.div(2).toFloat(),paint)
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         widthSize = w
         heightSize = h
+        radiusOfCircle = heightSize.div(2).times(0.5).toFloat()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -62,6 +85,7 @@ class LoadingButton @JvmOverloads constructor(context: Context, attrs: Attribute
         )
         widthSize = w
         heightSize = h
+        radiusOfCircle = heightSize.div(2).toFloat()
         setMeasuredDimension(w, h)
     }
 
